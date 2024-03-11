c163d68cbf2eb25af2e9405e571241e373b753d4
Jiakai Jiang <elvishscoutustc@outlook.com>
2024-03-11 15:28:04 +0800

    setup project

--- main.ts
+++ main.ts
@@ -2,16 +2,16 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface NetheriteSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: NetheriteSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class Netherite extends Plugin {
+	settings: NetheriteSettings;
 
 	async onload() {
 		await this.loadSettings();
@@ -108,9 +108,9 @@ class SampleModal extends Modal {
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: Netherite;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: Netherite) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
